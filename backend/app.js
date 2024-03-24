31d41c9ec74776df67ac69171523a087dc2aaae8
artopeu <arto.peurasaari@gmail.com>
2024-03-24 21:29:53 +0200

    RESTAPI_Account started.. Requires database.js before testing.

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+//var accountsRouter = require('./routes/account');
 
 var app = express();
 
