b9e425a372256a04b017fe0beb3e683f0222bd64
timopesola <156187311+timopesola@users.noreply.github.com>
2024-03-26 11:00:15 +0200

    Timo (#9)
    * test2.txt lisatty
    
    * tiedosto poistettu
    
    * atm+transaction peruskaskyt
    
    * dotenv pois ja example tilalle
    
    * dotenv pois

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var atmRouter = require ('./routes/atm');
+var transactionRouter = require ('./routes/transaction');
 
 var app = express();
 
@@ -16,5 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/atm', atmRouter);
+app.use('/transaction', transactionRouter);
 
 module.exports = app;
