71fd44485a98ae19f8fe6e72634a4dd18e64b230
Moravis <marko.oravisjarvi@gmail.com>
2024-03-26 10:29:39 +0200

    js kyselyitä päivitetty

---
+++ backend/models/customer_model.js
@@ -0,0 +1,28 @@
+const db=require('../database');
+
+const customer={
+    getAllCustomers(callback){
+        return db.query("SELECT * FROM customer WHERE id=?", [id],callback);
+    },
+    getOneCustomer(id,callback){
+        return db.query("SELECT * FROM customer WHERE id=?", [id],callback);
+    },
+    addCustomer(newCustomer,callback){
+        return db.query("INSTER INTO customer(customer_firstname, customer_lastname), values(?,?)",[
+            newCustomer.customer_firstname,
+            newCustomer.customer_lastname],
+            callback);
+    },
+    updateCustomer(id, updateCustomer,callback){
+        return db.query("UPDATE customer SET customer_firstname=?, customer_lastname=?",[
+            updateCustomer.customer_firstname,
+            updateCustomer.customer_lastname,
+            id],
+            callback);
+    },
+    deleteCustomer(id,callback){
+        return db.query("DELETE FROM customer WHERE customer_id=?",[id],callback);
+    }
+}
+
+modeule.exports=customer;
