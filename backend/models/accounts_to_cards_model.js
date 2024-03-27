f74491d4450f0d913a130148a1a4864e3cf87225
JereKortesalmi <t3koje03@students.oamk.com>
2024-03-27 11:24:29 +0200

    konflitit korjattu

--- backend/models/accounts_to_cards_model.js
+++ backend/models/accounts_to_cards_model.js
@@ -5,7 +5,7 @@ const accounts_to_cards={
         return db.query("SELECT * FROM accounts_to_cards", callback);
     },
     getOneAccounts_To_Cards(id,callback) {
-        return db.query("SELECT * FROM accounts_to_cards WHERE id=?",[id],callback);
+        return db.query("SELECT * FROM accounts_to_cards WHERE reference_id=?",[id],callback);
     },
     addAccounts_To_Cards(newAccounts_To_Cards,callback) {
         return db.query("INSERT INTO accounts_to_cards(reference_account_id, reference_card_id) VALUES(?,?)",[
@@ -14,14 +14,14 @@ const accounts_to_cards={
             callback);
     },
     updateAccounts_To_Cards(id,updateAccounts_To_Cards,callback) {
-        return db.query("UPDATE account_to_cards SET reference_account_id=?, reference_card_id=? WHERE reference_id=?",
-        [updateAccounts_To_Cards.reference_account_idaccount_id, 
+        return db.query("UPDATE accounts_to_cards SET reference_account_id=?, reference_card_id=? WHERE reference_id=?",
+        [updateAccounts_To_Cards.reference_account_id, 
             updateAccounts_To_Cards.reference_card_id,
             id],
             callback);
     },
     deleteAccounts_To_Cards(id,callback) {
-        return db.query("DELETE FROM account_to_cards WHERE reference_id=?", [id], callback);
+        return db.query("DELETE FROM accounts_to_cards WHERE reference_id=?", [id], callback);
     }
 }
 
