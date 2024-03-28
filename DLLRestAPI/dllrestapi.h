58b2100719a23a796ec245e8237ef03c94d60fa3
timopesola <timo.pesola@gmail.com>
2024-03-28 13:49:02 +0200

    tilitapahtumat 'taulukossa'

--- DLLRestAPI/dllrestapi.h
+++ DLLRestAPI/dllrestapi.h
@@ -1,12 +1,15 @@
 #ifndef DLLRESTAPI_H
 #define DLLRESTAPI_H
-
+#include <QDebug>
 #include "DLLRestAPI_global.h"
+#include "transactions.h"
 
 class DLLRESTAPI_EXPORT DLLRestAPI
 {
 public:
     DLLRestAPI();
 };
+
+void DLLRESTAPI_EXPORT huuhaa();
 
 #endif // DLLRESTAPI_H
