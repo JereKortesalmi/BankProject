bc46ecdeff8e4b533fad348637478e569206c725
timopesola <timo.pesola@gmail.com>
2024-04-11 10:57:33 +0300

    undo 3

--- backend/models/credit_or_debit_model.js
+++
@@ -1,9 +0,0 @@
-const db=require('../database');
-
-const credit_or_debit={
-    determineCardType(card_number,callback){
-        return db.query("SELECT card_type FROM card WHERE card_number=?",[card_number],callback);
-    }
-}
-
-module.exports=credit_or_debit;
